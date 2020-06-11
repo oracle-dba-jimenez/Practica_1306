bb58a6d6c82210c06868b203b7eada38eae7624a
oracle-dba-jimenez <66656638+oracle-dba-jimenez@users.noreply.github.com>
2020-06-10 21:23:05 -0400

    Primer commit del curso

--- index.js
+++ index.js
@@ -2,13 +2,18 @@
 Practica del dia 13 de junio de 2020
  Realizado por Ing. Miguel Angel Jiménez
  */
+function practica(){
 
- // Definicion de variables
 let nombre = "Miguel Angel";
 let apellido_p = "Jiménez";
 let apellido_m = "Solano";
 
-// Despliegue de alertas
    alert("Mi nombre es: "+nombre);
    alert("Mi apellido paterno es: "+apellido_p);
    alert("Mi apellido materno es: "+apellido_m);
+};
+
+
+ // Definicion de variables
+
+
